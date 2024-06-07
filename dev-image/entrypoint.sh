34359ea763e62de9398e91c538df856a9bd3d8fa
Juraj Brabec <juraj@brabec.sk>
2024-06-07 17:55:17 +0200

    Modifies entrypoint.sh

--- dev-image/entrypoint.sh
+++ dev-image/entrypoint.sh
@@ -5,6 +5,9 @@ echo "DEV Image - Default shell for current user ID in passwd file"
 # usermod -s /bin/bash $(whoami)
 cat /etc/passwd | grep $(whoami)
 
+echo "DEV Image - Reseting permissions"
+chown -R openshift:root /opt/code-base; chmod -R 775 /opt/code-base
+
 echo "DEV Image - Starting daemon"
 # Create daemon process
 while true
