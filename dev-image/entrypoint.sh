7ba7c7ba07c97cc0827b2b7e91dafedf6330331b
Juraj Brabec <juraj@brabec.sk>
2024-06-07 14:40:40 +0200

    Handles passwd

--- dev-image/entrypoint.sh
+++ dev-image/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# change default bash for current user id in passwd file
+echo "DEV Image - Changing default shell for current user ID in passwd file"
+usermod -s /bin/bash $(whoami)
+cat /etc/passwd | grep $(whoami)
+
 echo "DEV Image - Starting daemon"
 # Create daemon process
 while true
