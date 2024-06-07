5e6ccba709f69f5f84de54c2ee689478cd3d82e6
Juraj Brabec <juraj@brabec.sk>
2024-06-07 17:10:09 +0200

    user mess

--- dev-image/entrypoint.sh
+++ dev-image/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# change default bash for current user id in passwd file
-echo "DEV Image - Changing default shell for current user ID in passwd file"
-usermod -s /bin/bash $(whoami)
+# default bash for current user id in passwd file
+echo "DEV Image - Default shell for current user ID in passwd file"
+# usermod -s /bin/bash $(whoami)
 cat /etc/passwd | grep $(whoami)
 
 echo "DEV Image - Starting daemon"
