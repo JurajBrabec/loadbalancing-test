7d2e7608a9bb45ca3d7c72436200e0e4ff2f6bec
Juraj Brabec <juraj@brabec.sk>
2024-06-10 14:48:00 +0200

    Refactoring

---
+++ dev-image/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "DEV Image deployment"
+
+
--- dev-image/entrypoint.sh
+++ dev-image/entrypoint.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-echo "DEV Image"
+echo "DEV Image entrypoint"
+
+if [[ ! -d ${HOME} ]]; then
+    echo "Creating home"
+    mkdir -p ${HOME}
+fi
+# Copy .npmrc file to home folder
+cp /.init/.npmrc ${HOME}
 
 # Add group write permissions to persistent volume folders to be able to delete old files when Openshift user ID changes again
 echo "Reseting permissions for home"
 chown -R openshift:root ${HOME} # >/dev/null 2>&1
 chmod -R g+w ${HOME} # >/dev/null 2>&1
-echo "Reseting permissions for repository"
-chmod -R g+w ${HOME}/../excalibur-v4 # >/dev/null 2>&1
-echo "Reseting permissions for volumes"
-chmod -R g+w ${HOME}/../volumes # >/dev/null 2>&1
 
-echo "Starting daemon $(date)"
+if [[ ! -d /opt/code-base/excalibur-v4 ]]; then
+    echo "Creating repository"
+    mkdir -p /opt/code-base/excalibur-v4
+fi
+echo "Reseting permissions for repository"
+chmod -R g+w /opt/code-base/excalibur-v4 # >/dev/null 2>&1
+
+
+if [[ ! -d /opt/code-base/volumes ]]; then
+    echo "Creating volumes"
+    mkdir -p /opt/code-base/volumes/{dashboard-static-files,database,grafana-data,loki-data,pam-client-static-files,pam-recordings}
+fi
+echo "Reseting permissions for volumes"
+chmod -R g+w /opt/code-base/volumes # >/dev/null 2>&1
 
 # Create daemon process
+echo "Starting daemon $(date)"
+
 while true
 do
-sleep 1
+    sleep 1
 done
