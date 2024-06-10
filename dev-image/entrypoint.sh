00c3204dd7f88a74af469a4a6b4b8508ed162a27
Juraj Brabec <juraj@brabec.sk>
2024-06-10 10:36:18 +0200

    Modifies entrypoint.sh

--- dev-image/entrypoint.sh
+++ dev-image/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "DEV Image"
 
-# Add group write permissions to $HOME folder to be able to delete old files when Openshift user ID changes again
-echo "Reseting permissions for ${HOME}"
-
+# Add group write permissions to persistent volume folders to be able to delete old files when Openshift user ID changes again
+echo "Reseting permissions for home"
 chown -R openshift:root ${HOME} # >/dev/null 2>&1
 chmod -R g+w ${HOME} # >/dev/null 2>&1
+echo "Reseting permissions for repository"
 chmod -R g+w ${HOME}/../excalibur-v4 # >/dev/null 2>&1
+echo "Reseting permissions for volumes"
 chmod -R g+w ${HOME}/../volumes # >/dev/null 2>&1
 
 echo "Starting daemon $(date)"
